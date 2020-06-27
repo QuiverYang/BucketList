b3f0d467b5c47b0d67118f60831155814db4e30a
YangMongLin <m778677@yahoo.com.tw>
2020-06-27 14:46:19 +0800

    Add - login screen

---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('go to sign up page'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/signUp');
+              },
+            )
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
