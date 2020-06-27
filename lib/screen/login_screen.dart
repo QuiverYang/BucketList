76ef09aba63642bf7602eb2400e3e511bc5a2ed6
YangMongLin <m778677@yahoo.com.tw>
2020-06-27 16:26:10 +0800

    Add - login screen

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bucketlist/utilities/constant.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -12,21 +13,77 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              child: Text('go to sign up page'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/signUp');
-              },
-            )
-          ],
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/appBg@3x.png"),
+            fit: BoxFit.cover,
+          ),
+          color: Color.fromARGB(255, 117, 53, 83),
+        ),
+//        constraints: BoxConstraints.expand(),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * 0.1),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Admin Quests',
+                style: TextStyle(color: kThemeColor, fontSize: 40),
+              ),
+              SizedBox(height: height * 0.12),
+              Image.asset(
+                'images/earth@3x.png',
+                width: width * 0.7,
+              ),
+              SizedBox(height: height * 0.06),
+              FlatButton(
+                color: kThemeColor,
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Sign up',
+                    style: TextStyle(color: kTextColor, fontSize: 20),
+                  ),
+                ),
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  side: BorderSide(color: kThemeColor, width: 2),
+                ),
+              ),
+              SizedBox(height: 5),
+              FlatButton(
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: kThemeColor, fontSize: 20),
+                  ),
+                ),
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  side: BorderSide(color: kThemeColor, width: 2),
+                ),
+              ),
+              SizedBox(height: 15),
+              Container(
+                width: double.infinity,
+                child: Text(
+                  'Forgot Account??',
+                  style: TextStyle(color: kThemeColor),
+                  textAlign: TextAlign.right,
+                ),
+              )
+            ],
+          ),
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
