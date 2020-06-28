b1156089d41f67ac41a48c540d112fb70b9a3fc8
Rebecca <rebecca.h@cathayholdings.com.tw>
2020-06-28 15:19:58 +0800

    Edit - fake data wording

--- lib/utilities/fakedata.dart
+++ lib/utilities/fakedata.dart
@@ -1,46 +1,41 @@
 const List<String> categories = [
-  'Environment',
-  'Family',
+  'Family & Friends',
   'Work',
-  'Entertainment',
   'Health',
-  'Self-growth'
+  'Wealth',
+  'Interests'
 ];
 
 const Map<String, List<String>> questTitles1 = {
-  'Environment': [
-    'Clean Beach',
-    'Reduce Waste',
-    'Global Warming',
-    'Planting Trees',
-  ],
-  'Family': [
-    'Wash My Cat',
-    'Propose',
-    'Raise a Child',
-    'Housework',
-    'Visiting Grandparents',
+  'Family & Friends': [
+    'Make a plan for buying a house',
+    'Upgrade the security system of our home',
+    'Celebrate someone\'s birthday',
+    'Find a good teacher for the kids',
+    'Host a party this weekend ',
   ],
   'Work': [
-    'Write an App',
-    'Presentation Skill',
-    'Communication Skill',
-    'Financial Knowledge',
-  ],
-  'Entertainment': [
-    'Aurora',
-    'Live in a Woods',
-    'Parachute',
-    'Rock Climbing',
+    'Get promotion by the end of this year',
+    'Accomplish the project goals before the deadline',
+    'Be a good mentor',
+    'Learn new professional skills',
+    'Buy yourself a better car/motor',
   ],
   'Health': [
-    'Fitness',
-    'Lose Weight',
+    'Charge mechanical parts 8 hours per day',
+    'Join a body training club',
+    'Make a body part maintenance plan',
   ],
-  'Self-growth': [
-    'Reading a Book',
-    'Learn a Skill',
-    'Learn Cooking',
-    'Photography',
+  'Wealth': [
+    'Save money for a more substantial body part',
+    'Get an insurance plan',
+    'Start to track your spending',
   ],
+  'Interests': [
+    'Travel to a planet',
+    'Cook dinner once a week',
+    'Get a plant and take care of it',
+    'Join a club',
+    'Watch movies that you always want to watch',
+  ]
 };
